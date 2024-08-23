7f49e8bdf90b85cb862cd00a52956e4b4da4cfec
avika.surana <avikasurana2005@gmail.com>
2024-08-23 11:54:38 +0530

    updating files

--- c2.js
+++ c2.js
@@ -139,3 +139,4 @@ console.log(result)
 
 
 
+ 
---
+++ c5.js
@@ -0,0 +1,16 @@
+ // console.log(document)
+
+let mydiv=document.getElementById("main")
+// console.log(mydiv)
+// console.log(mydiv.id)
+// mydiv.id="container"
+// mydiv.className="heading"
+// console.log(mydiv)
+console.log(mydiv.style.width="200px")
+console.log(mydiv.style.height="200px")
+console.log(mydiv.style.backgroundColor="red")
+console.log(mydiv.style.border="2px dotted black")
+console.log(mydiv.style.borderRadius="20px")
+console.log(mydiv.getAttribute("class"))//sirf getAtt mai he direct class access hoskti hai
+console.log(mydiv.setAttribute("id","container"))
+
